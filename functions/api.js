c8eb560b1ff53f3b51a0949e3c4a8245826f4220
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-01-01 04:16:43 +0500

    Data validations for login and sign-up. Added products list endpoint

--- functions/api.js
+++ functions/api.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 // const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
 const faunadb = require('faunadb');
 const q = faunadb.query;
@@ -48,29 +49,93 @@ app.use(bodyParser.urlencoded({ extended: true }));
 router.post('/auth', authMiddleware, (req, res) => {
   const { user: { iat, exp, ...user } } = req;
   
+
+  const token = jwt.sign({
+    id: user.id,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    phoneNumber: user.phoneNumber,
+  }, process.env.JWT_SECRET_TOKEN, { expiresIn: "5h" });
+
   res.json({
     user,
-    token: 'XLJAJSLWEDBsd123KGYSDGCJDBQSHD123GBASBDsbdhasd'
+    token
   });
 });
 
-router.post('/auth/login', (req, res) => {
+router.post('/auth/login', async (req, res) => {
   try {
+    const errors = {};
     const { email, password } = req.body;
 
-    if (!(email && password)) {
+    if(!validator.isEmail(email)){
+      errors.email = 'Field "email" is not valid email!'
+    }
+
+    if(validator.isEmpty(password)){
+      errors.password = 'Field "password" is requied!'
+    }
+
+    if(Object.keys(errors).length){
       throw ({
         message: 'Payload fields validation error!',
-        errors: {
-          email: !email ? 'Field "email" is requied!' : undefined,
-          password: !password ? 'Field "password" is requied!' : undefined
-        }
+        errors
       })
     }
-      
+
+    const { userExist, user } = await client.query(
+      q.Let(
+        {
+          findUser: q.Select(
+            ['data'],
+            q.Map(
+              q.Paginate(q.Match(q.Index('userByEmail'), email), { size: 1 }),
+              q.Lambda('x', q.Get(q.Var('x')))
+            ),
+            []
+          ),
+          userExist: q.GT(q.Count(q.Var('findUser')), 0),
+        },
+        {
+          userExist: q.Var("userExist"),
+          user: q.If(
+            q.Var('userExist'),
+            {
+              id: q.Select([0, "ref", "id"], q.Var('findUser'), undefined),
+              firstName: q.Select([0, "data", "firstName"], q.Var('findUser'), undefined),
+              lastName: q.Select([0, "data", "lastName"], q.Var('findUser'), undefined),
+              email: q.Select([0, "data", "email"], q.Var('findUser'), undefined),
+              phoneNumber: q.Select([0, "data", "phoneNumber"], q.Var('findUser'), undefined),
+              password: q.Select([0, "data", "password"], q.Var('findUser'), undefined),
+            },
+            {}
+          )
+        }
+      )
+    );
+
+    if(!userExist || user.password !== password){
+      throw ({
+        message: 'Incorrect username or password!',
+        user: {}
+      });
+    }
+
+    const token = jwt.sign({
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+    }, process.env.JWT_SECRET_TOKEN, { expiresIn: "5h" });
+
+    delete user.password;
+    
     res.json({
-      email,
-      token: 'XLJAJSLWEDBsd123KGYSDGCJDBQSHD123GBASBDsbdhasd'
+      message: "User is logged in successfully!",
+      user,
+      token
     });
   } catch (error) {
     res.json(error)
@@ -79,17 +144,41 @@ router.post('/auth/login', (req, res) => {
 
 router.post('/auth/sign-up', async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
+    const errors = {};
+    const { firstName, lastName, email, password, phoneNumber } = req.body;
 
-    if (!(firstName && lastName && email && password)) {
+    if(validator.isEmpty(firstName || '')){
+      errors.firstName = 'Field "firstName" is requied!';
+    }
+
+    if(validator.isEmpty(lastName || '')){
+      errors.lastName = 'Field "lastName" is requied!';
+    }
+
+    if(!validator.isEmail(email || '')){
+      errors.email = 'Field "email" is not valid email!';
+    }
+
+    if(!validator.isStrongPassword(password || '')){
+      errors.password = 'Field "password" is not strong enough!';
+    }
+
+    if(validator.isEmpty(password || '')){
+      errors.password = 'Field "password" is requied!';
+    }
+
+    if(!validator.isMobilePhone(phoneNumber || '')){
+      errors.phoneNumber = 'Field "Phone number" is not valid!';
+    }
+
+    if(validator.isEmpty(phoneNumber || '')){
+      errors.phoneNumber = 'Field "Phone number" is requied!';
+    }
+
+    if(Object.keys(errors).length){
       throw ({
         message: 'Payload fields validation error!',
-        errors: {
-          firstName: !firstName ? 'Field "firstName" is requied!' : undefined,
-          lastName: !lastName ? 'Field "lastName" is requied!' : undefined,
-          email: !email ? 'Field "email" is requied!' : undefined,
-          password: !password ? 'Field "password" is requied!' : undefined
-        }
+        errors
       })
     }
 
@@ -117,7 +206,8 @@ router.post('/auth/sign-up', async (req, res) => {
                 firstName,
                 lastName,
                 email,
-                password
+                password,
+                phoneNumber
               }
             }),
             {}
@@ -132,6 +222,7 @@ router.post('/auth/sign-up', async (req, res) => {
               firstName: q.Select(["data", "firstName"], q.Var('createUser'), undefined),
               lastName: q.Select(["data", "lastName"], q.Var('createUser'), undefined),
               email: q.Select(["data", "email"], q.Var('createUser'), undefined),
+              phoneNumber: q.Select(["data", "phoneNumber"], q.Var('createUser'), undefined),
             },
             {}
           )
@@ -151,17 +242,49 @@ router.post('/auth/sign-up', async (req, res) => {
       email: user.email,
       firstName: user.firstName,
       lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
     }, process.env.JWT_SECRET_TOKEN, { expiresIn: "5h" });
 
     res.json({
       message: "User is signed up successfully!",
       user,
       token
-    })
+    });
   } catch (error) {
     res.json(error)
   }
 })
+
+router.get('/products', async (req, res) => {  
+  
+  const { data = [] } = await client.query(
+    q.Let(
+      {
+        list: q.Map(
+          q.Paginate(q.Documents(q.Collection('products')), { size: 99999 }),
+          q.Lambda(x => q.Get(x))
+        )
+      },
+      {
+        data: q.Map(
+          q.Select(["data"], q.Var("list"), []),
+          q.Lambda(product => {
+            return ({
+              id: q.Select(["ref", "id"], product, undefined),
+              name: q.Select(["data", "name"], product, undefined),
+              deliveryTime: q.Select(["data", "deliveryTime"], product, undefined),
+              minimalOrder: q.Select(["data", "minimalOrder"], product, undefined),
+              image: q.Select(["data", "image"], product, undefined),
+              tags: q.Select(["data", "tags"], product, []),
+            })
+          }
+        ))
+      }
+    )
+  );
+  
+  res.json({ data });
+});
 
 app.use('/api', router);
 app.use('*', (_, res) => res.send(404));
